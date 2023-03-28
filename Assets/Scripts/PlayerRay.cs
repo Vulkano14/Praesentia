60ef309790f9ea04a876ccc508c50315c89be6b4
Vulkano14 <krzysztofkwiecien707@gmail.com>
2023-03-28 23:53:12 +0200

    add first part script flashlight

---
+++ Assets/Scripts/PlayerRay.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerRay : MonoBehaviour
+{
+    public LayerMask pickLayerMask;
+    public Transform playerCameraTransform;
+    public GameObject pickUpUI;
+    
+    [Min(1)]
+    public float hitRange = 3;
+
+    public RaycastHit hit;
+
+    public void Update()
+    {
+        if(hit.collider != null)
+        {
+            hit.collider.GetComponent<Highlight>()?.togHighlight(false);
+            pickUpUI.SetActive(false);
+        }
+        if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickLayerMask))
+        {
+            hit.collider.GetComponent<Highlight>()?.togHighlight(true);
+            pickUpUI.SetActive(true);
+        }
+    }
+
+}
